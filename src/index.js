06cdace304372b6d589e4447b27b95642ecb0c1f
iyc <82129932+interstellarYachtClub@users.noreply.github.com>
2022-06-13 19:13:06 -0400

    Updated with some initial filechanges

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import BuddyUpApp from './App';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+    <BuddyUpApp />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
